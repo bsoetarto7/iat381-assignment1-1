5f2f1822bd134009839d974d5b5446577aa3f708
Laura Li <xll@sfu.ca>
2015-02-07 01:49:15 -0800

    comments added to the appropriate pages

--- js/FlightDetailedCtrl.js
+++ js/FlightDetailedCtrl.js
@@ -1,20 +1,21 @@
 flightinfoApp.controller('FlightDetailedCtrl', function ($scope, InfoService) {
+  // geting the information from the service
   $scope.flightInfo = InfoService.getFilter();
   $scope.message = InfoService.messageDisplay();
     if(!InfoService.messageDisplay()){
         $scope.myValue = true;
     }
 
+  // splite the items and display them later
   $scope.inputDepartLocation = String(InfoService.getFromInfo()).split("-")[0];
   $scope.inputArriveLocation = String(InfoService.getDestinationInfo()).split("-")[0];
- 
-  $scope.inputLeavingTime = InfoService.getLeavingInfo();
-  $scope.inputReturningTime = InfoService.getReturningInfo();
 
+  // have the option to have the ordering
   $scope.orderProp = 'Air_line';
   $scope.orderProp = 'leaving';
   $scope.orderProp = 'price';
 
+  // the add the information from favorate button to the service
   $scope.getFavourites = function($item){
     InfoService.addFavourites($item);
   };
--- js/favouriteCtrl.js
+++ js/favouriteCtrl.js
@@ -1,6 +1,10 @@
 flightinfoApp.controller('FavouriteCtrl', function ($scope, InfoService) {
+	// get information from the service page 
 	$scope.message='';
 	$scope.flightInfo= InfoService.getFavourites();
+
+	// check the list see if there is nothing in the list,
+	// show the user additional steps need to be add 
 	if(InfoService.getFavourites().length == 0){
 		$scope.message = "There is nothing in your favourite list";
 	}
@@ -13,6 +17,7 @@ flightinfoApp.controller('FavouriteCtrl', function ($scope, InfoService) {
          $scope.myValue= true;
     }
     
+    // remove items when the cross button is clicked
 	$scope.removeFavourites = function($item){
     	InfoService.removeFavourites($item);
     	if(InfoService.getFavourites().length == 0){
